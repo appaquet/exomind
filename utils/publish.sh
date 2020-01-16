9a4fbe1882ebd7496bff1e92b140ee1334806dd8
Andre-Philippe Paquet <appaquet@gmail.com>
2020-01-15 20:29:05 -0500

    Publishing

---
+++ utils/publish.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+set -e
+CUR_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+cd $CUR_DIR/../
+
+CRATES=("common" "schema" "transport" "data" "index"  "." "exo")
+
+for CRATE in "${CRATES[@]}"; do
+  pushd $CUR_DIR/../$CRATE
+  cargo publish ${@:1}
+  popd
+done
