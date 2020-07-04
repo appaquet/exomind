56fc9e666de3e3f3e24d85801ac53767f4175919
Andre-Philippe Paquet <appaquet@gmail.com>
2020-07-04 18:30:10 -0400

    Bump web examples deps

--- examples/web/src/index.tsx
+++ examples/web/src/index.tsx
@@ -1,6 +1,3 @@
-import "core-js/stable";
-import "regenerator-runtime/runtime";
-
 import { Exocore, exocore } from "exocore";
 import React, { ChangeEvent } from 'react';
 import ReactDOM from 'react-dom';
