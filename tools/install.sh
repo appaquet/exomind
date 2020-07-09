07b2cef896bd312d6350f0728c2340c6420251e3
Andre-Philippe Paquet <appaquet@gmail.com>
2020-07-08 21:01:04 -0400

    Lock dependencies for exo build

--- tools/install.sh
+++ tools/install.sh
@@ -3,6 +3,6 @@ set -e
 CUR_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 cd "$CUR_DIR"
 
-pushd $CUR_DIR/../exo
-cargo install ${@:1}  --path .
+pushd $CUR_DIR/..
+cargo install ${@:1} --path exo --locked
 popd
