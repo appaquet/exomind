1b27b529c5f4e7e96b64b887479893c4ed5bf056
Andre-Philippe Paquet <appaquet@gmail.com>
2020-07-11 12:05:48 -0400

    iOS version build fix

--- clients/ios/tools/generate.sh
+++ clients/ios/tools/generate.sh
@@ -5,18 +5,14 @@ CUR_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 EXOCORE_IOS_ROOT="$CUR_DIR/../"
 EXOCORE_ROOT="$EXOCORE_IOS_ROOT/../../"
 
-pushd $EXOCORE_IOS_ROOT
-cbindgen --config cbindgen.toml --crate exocore-client-ios --output swift/exocore.h
-popd
+cbindgen --config $EXOCORE_IOS_ROOT/cbindgen.toml --crate exocore-client-ios --output $EXOCORE_IOS_ROOT/swift/exocore.h
 
-pushd $EXOCORE_IOS_ROOT
-rm -rf swift/protos
-mkdir -p swift/protos
+rm -rf $EXOCORE_IOS_ROOT/swift/protos
+mkdir -p $EXOCORE_IOS_ROOT/swift/protos
 protoc -I "$EXOCORE_ROOT/protos/" \
   --swift_opt=Visibility=Public \
-  --swift_out=swift/protos/ \
+  --swift_out=$EXOCORE_IOS_ROOT/swift/protos/ \
   $EXOCORE_ROOT/protos/exocore/core/*.proto \
   $EXOCORE_ROOT/protos/exocore/apps/*.proto \
   $EXOCORE_ROOT/protos/exocore/index/*.proto \
   $EXOCORE_ROOT/protos/exocore/test/*.proto
-popd
