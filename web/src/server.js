aa4e85dbb5c7bc33f60875c83d27fc25b9cd2c34
Andre-Philippe Paquet <appaquet@gmail.com>
2020-08-13 21:31:17 -0400

    Bump & clean

--- web/src/server.js
+++ web/src/server.js
@@ -4,27 +4,9 @@ const express = require('express');
 const cookieParser = require('cookie-parser');
 
 var server = express();
-server.set('port', (process.env.PORT || 8000));
+server.set('port', (process.env.PORT || 8080));
 server.use(express.static(path.join(__dirname)));
 server.use(cookieParser());
-
-server.get('/logout', (req, res) => {
-  res.clearCookie('sid');
-  res.send('<script language="javascript">window.location.href="/";</script>');
-});
-
-server.get('/login', (req, res) => {
-  res.redirect('/v1/auth/google/url');
-});
-
-server.get('/switch', (req, res) => {
-  let cookieOptions = {
-    expires: new Date(Date.now() + 86400000*365)
-  };
-  res.cookie('bid', req.query['b'], cookieOptions);
-  res.cookie('fid', req.query['f'], cookieOptions);
-  res.send('<script language="javascript">window.location.href="/";</script>');
-});
 
 // handle push state
 server.get('/*', function (req, res) {
