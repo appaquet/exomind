74f5d66aba172982e3b737d57cab88fc098b767e
Andre-Philippe Paquet <appaquet@gmail.com>
2020-12-23 09:08:36 -0500

    Fix version bump

--- tools/bump_version.sh
+++ tools/bump_version.sh
@@ -10,8 +10,9 @@ if [[ -z $VERSION ]]; then
 fi
 
 ROOT_DIR="$CUR_DIR/.."
+VERSION_RE="[0-9]+\.[0-9]+\.[0-9]+"
 
-sed -i.bak "s/\(spec\.version.*=\).*/\1 '$VERSION'/g" $ROOT_DIR/Exocore.podspec
+sed -i.bak -E "s/^([[:space:]]+spec\.version.*=).*/\1 '$VERSION'/" $ROOT_DIR/Exocore.podspec
 sed -i.bak "s/\(\"version\":\).*/\1 \"$VERSION\",/g" $ROOT_DIR/package.json
 
 CRATES=( \
@@ -29,6 +30,6 @@ CRATES=( \
 
 for CRATE in "${CRATES[@]}"; do
   TOML_PATH="$ROOT_DIR/${CRATE}/Cargo.toml"
-  sed -i.bak "s/^\(version = \).*/\1\"$VERSION\"/g" $TOML_PATH
-  sed -i.bak -E "s/(exocore.*version.*\")([0-9]+\.[0-9]+\.[0-9]+)(\".*)/\1$VERSION\3/g" $TOML_PATH
+  sed -i.bak "s/^\(version = \).*/\1\"${VERSION}\"/g" $TOML_PATH
+  sed -i.bak -E "s/(exocore.*version.*\")(${VERSION_RE})(\".*)/\1${VERSION}\3/g" $TOML_PATH
 done
