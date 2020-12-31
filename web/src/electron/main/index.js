ce72e199a9a707ad37771962e4bae2029851c425
Andre-Philippe Paquet <appaquet@gmail.com>
2020-12-31 11:15:05 -0500

    Clippy 1.49 fixes

--- web/src/electron/main/index.js
+++ web/src/electron/main/index.js
@@ -6,23 +6,22 @@ import * as path from 'path'
 import { format as formatUrl } from 'url'
 
 const isDevelopment = process.env.NODE_ENV !== 'production'
+const autoDevTool = false;
 
-// global reference to mainWindow (necessary to prevent window from being garbage collected)
-let mainWindow
 
-function createMainWindow() {
+function createNewWindow() {
   const window = new BrowserWindow({
     webPreferences: { nodeIntegration: true },
     autoHideMenuBar: true,
   });
   window.setMenuBarVisibility(false);
 
-  if (isDevelopment) {
-    window.webContents.openDevTools()
+  if (isDevelopment && autoDevTool) {
+    window.webContents.openDevTools();
   }
 
   if (isDevelopment) {
-    window.loadURL(`http://localhost:${process.env.ELECTRON_WEBPACK_WDS_PORT}`)
+    window.loadURL(`http://localhost:${process.env.ELECTRON_WEBPACK_WDS_PORT}`);
   }
   else {
     window.loadURL(formatUrl({
@@ -32,25 +31,31 @@ function createMainWindow() {
     }))
   }
 
+  return window;
+}
+
+let mainWindow;
+function createMainWindow() {
+  const window = createNewWindow();
   window.on('closed', () => {
-    mainWindow = null
+    mainWindow = null;
   })
 
   window.webContents.on('devtools-opened', () => {
-    window.focus()
+    window.focus();
     setImmediate(() => {
-      window.focus()
+      window.focus();
     })
   })
 
-  return window
+  return window;
 }
 
 // quit application when all windows are closed
 app.on('window-all-closed', () => {
   // on macOS it is common for applications to stay open until the user explicitly quits
   if (process.platform !== 'darwin') {
-    app.quit()
+    app.quit();
   }
 })
 
@@ -63,22 +68,11 @@ app.on('activate', () => {
 
 // create main BrowserWindow when electron is ready
 app.on('ready', () => {
-  mainWindow = createMainWindow()
+  mainWindow = createMainWindow();
 })
 
 ipcMain.on('open-popup', (event, navPath) => {
-  const window = new BrowserWindow({ webPreferences: { nodeIntegration: true } })
-  window.setMenuBarVisibility(isDevelopment);
-
-  if (isDevelopment) {
-    window.loadURL(`http://localhost:${process.env.ELECTRON_WEBPACK_WDS_PORT}`)
-  } else {
-    window.loadURL(formatUrl({
-      pathname: path.join(__dirname, 'index.html'),
-      protocol: 'file',
-      slashes: true
-    }))
-  }
+  const window = createNewWindow();
 
   window.webContents.on('did-finish-load', () => {
     window.webContents.send('navigate', navPath);
