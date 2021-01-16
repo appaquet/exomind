4e3bca02045d57456a1849081e0c09c838216c6c
Andre-Philippe Paquet <appaquet@gmail.com>
2021-01-16 12:40:45 -0500

    Bump exocore to 0.1.1 & update script

--- tools/bump_version.sh
+++ tools/bump_version.sh
@@ -9,11 +9,20 @@ if [[ -z $VERSION ]]; then
   exit 1
 fi
 
+SEDBIN="sed"
+if [[ "$OSTYPE" == "darwin"* ]]; then
+  if ! command -v gsed &> /dev/null; then
+      echo "on macos, you need to install gnused: brew install gnused"
+      exit
+  fi
+  SEDBIN="gsed"
+fi
+
 ROOT_DIR="$CUR_DIR/.."
 VERSION_RE="[0-9]+\.[0-9]+\.[0-9]+(|\-dev|\-pre[0-9]+)"
 
-sed -i.bak "s/\(\"version\":\).*/\1 \"$VERSION\",/g" $ROOT_DIR/web/package.json
-sed -i.bak "s/\(version:\).*/\1 $VERSION/g" $ROOT_DIR/app.yaml
+$SEDBIN -i.bak "s/\(\"version\":\).*/\1 \"$VERSION\",/g" $ROOT_DIR/web/package.json
+$SEDBIN -i.bak "s/\(version:\).*/\1 $VERSION/g" $ROOT_DIR/app.yaml
 
 CRATES=( \
   "." \
@@ -24,6 +33,6 @@ CRATES=( \
 
 for CRATE in "${CRATES[@]}"; do
   TOML_PATH="$ROOT_DIR/${CRATE}/Cargo.toml"
-  sed -i.bak "s/^\(version = \).*/\1\"$VERSION\"/g" $TOML_PATH
-  sed -i.bak -E "s/(exomind.*version.*\")(${VERSION_RE})(\".*)/\1${VERSION}\4/g" $TOML_PATH
+  $SEDBIN -i.bak "s/^\(version = \).*/\1\"$VERSION\"/g" $TOML_PATH
+  $SEDBIN -i.bak -E "s/(exomind.*version.*\")(${VERSION_RE})(\".*)/\1${VERSION}\4/g" $TOML_PATH
 done
