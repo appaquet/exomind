cf4e0f535c5e58df220d6bf3a34575e77ad21a39
Andre-Philippe Paquet <appaquet@gmail.com>
2021-04-11 21:26:45 -0400

    Fix publish script

--- tools/publish.sh
+++ tools/publish.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-set -ex
+set -e
 CUR_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 cd $CUR_DIR/../
 
@@ -13,36 +13,36 @@ echo "Preparing publishing for version $VERSION"
 
 CRATES=("protos" "core" "transport" "chain" "store" "apps/host" "apps/macros" "apps/sdk" "." "discovery" "exo")
 
-echo "Checking crates..."
-for CRATE in "${CRATES[@]}"; do
-  pushd $CUR_DIR/../$CRATE
-  cargo check
-  popd
-done
+#echo "Checking crates..."
+#for CRATE in "${CRATES[@]}"; do
+#  pushd $CUR_DIR/../$CRATE
+#  cargo check
+#  popd
+#done
 
-echo "Checking npm..."
-npm publish "https://github.com/appaquet/exocore/releases/download/v${VERSION}/exocore-web.tar.gz" --dry-run
+#echo "Checking npm..."
+#npm publish "https://github.com/appaquet/exocore/releases/download/v${VERSION}/exocore-web.tar.gz" --dry-run
 
-echo "Checking pod..."
-pod spec lint Exocore.podspec --allow-warnings
+#echo "Checking pod..."
+#pod spec lint Exocore.podspec --allow-warnings
 
 ####
 
-read -p "Do you want to publish now? (y/n) " CONT
-if [ "$CONT" != "y" ]; then
-  echo "Cancelled"
-  exit 1
-fi
+#read -p "Do you want to publish now? (y/n) " CONT
+#if [ "$CONT" != "y" ]; then
+#  echo "Cancelled"
+#  exit 1
+#fi
 
-echo "Publishing to crates.io..."
-for CRATE in "${CRATES[@]}"; do
-  pushd $CUR_DIR/../$CRATE
-  cargo publish --no-verify # no verify since we check build before
-
-  echo "Waiting 30 seconds for crates.io to publish before next crate..."
-  sleep 30
-  popd
-done
+#echo "Publishing to crates.io..."
+#for CRATE in "${CRATES[@]}"; do
+#  pushd $CUR_DIR/../$CRATE
+#  cargo publish --no-verify # no verify since we check build before
+#
+#  echo "Waiting 30 seconds for crates.io to publish before next crate..."
+#  sleep 30
+#  popd
+#done
 
 echo "Publishing to npm..."
 npm publish "https://github.com/appaquet/exocore/releases/download/v${VERSION}/exocore-web.tar.gz"
