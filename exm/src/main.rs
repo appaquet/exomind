eedbe2798a41fe590cc3054050b86d589ba5e28b
Andre-Philippe Paquet <appaquet@gmail.com>
2021-06-05 21:09:52 -0400

    fix gha

--- exm/src/main.rs
+++ exm/src/main.rs
@@ -10,12 +10,12 @@ async fn main() {
     let opt: cli::Options = cli::Options::from_args();
     exocore::core::logging::setup::<String>(Some(LevelFilter::from_str(&opt.log).unwrap()), None);
 
-    let client = exocore::client::Client::from_node_config_file(opt.node_conf_path())
-        .await
-        .expect("Couldn't create client from config");
-
     match &opt.command {
         cli::Command::Gmail(gmail_opt) => {
+            let client = exocore::client::Client::from_node_config_file(opt.node_conf_path())
+                .await
+                .expect("Couldn't create client from config");
+
             exomind_gmail::handle(client, opt.directory(), gmail_opt).await
         }
         cli::Command::Version => {
