2a8f606f35242ad8cc1d9e2422ad5771983d4f94
Andre-Philippe Paquet <appaquet@gmail.com>
2021-06-06 20:50:22 -0400

    fix publish

--- tools/publish.sh
+++ tools/publish.sh
@@ -3,7 +3,7 @@ set -e
 CUR_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 cd $CUR_DIR/../
 
-CRATES=("app" "protos" "integrations/gmail" "." "exm")
+CRATES=("protos" "app" "integrations/gmail" "." "exm")
 
 echo "Checking crates..."
 for CRATE in "${CRATES[@]}"; do
