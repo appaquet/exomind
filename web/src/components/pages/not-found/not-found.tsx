bd440ce516d63926608522e31f536fb70e3eb911
Andre-Philippe Paquet <appaquet@gmail.com>
2023-05-08 20:51:43 -0400

    Bump deps (#2070)

--- web/src/components/pages/not-found/not-found.tsx
+++ web/src/components/pages/not-found/not-found.tsx
@@ -2,7 +2,7 @@
 import React from 'react';
 import './not-found.less';
 
-export default function NotFound(): JSX.Element {
+export default function NotFound(): React.JSX.Element {
     return (
       <div>Oops, page not found</div>
     );
