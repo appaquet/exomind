14b188165e692898f950a64e44865a5055b97c43
Andre-Philippe Paquet <appaquet@gmail.com>
2024-09-08 20:57:14 -0400

    ci

--- exocore/tools/install.sh
+++ exocore/tools/install.sh
@@ -3,5 +3,5 @@ set -ex
 CUR_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 pushd $CUR_DIR/../exo
-cargo install ${@:1} --path . --locked
+cargo install "${@:1}" --path . --locked
 popd
--- tools/coverage.sh
+++ tools/coverage.sh
@@ -2,7 +2,7 @@
 set -ex
 CUR_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
-REPO_ROOT="$CUR_DIR/../.."
+REPO_ROOT="$CUR_DIR/.."
 
 OUTPUT=${1:-html}
 
@@ -10,7 +10,7 @@ OUTPUT_ARGS=""
 if [[ "$OUTPUT" == "html" ]]; then
 	OUTPUT_ARGS="--html"
 else
-	OUTPUT_ARGS="--lcov --output-path $CUR_DIR/../lcov.info"
+	OUTPUT_ARGS="--lcov --output-path $REPO_ROOT/lcov.info"
 fi
 
 rustup component add llvm-tools-preview
