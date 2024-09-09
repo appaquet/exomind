f5c0ba7eb54bf5d6ea9b80f84ba1b96050fb562b
Andre-Philippe Paquet <appaquet@gmail.com>
2024-09-08 20:20:21 -0400

    ci

--- exocore/apps/tools/build_fixtures.sh
+++ exocore/apps/tools/build_fixtures.sh
@@ -3,8 +3,8 @@ set -e
 CUR_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 APPS_DIR="$CUR_DIR/../"
-EXOCORE_ROOT="$APPS_DIR/../"
+REPO_ROOT="$APPS_DIR/../../"
 
 pushd $APPS_DIR/example/
 cargo build --target wasm32-wasi --release
-cp $EXOCORE_ROOT/target/wasm32-wasi/release/exocore_apps_example.wasm $APPS_DIR/host/fixtures/example.wasm
+cp $REPO_ROOT/target/wasm32-wasi/release/exocore_apps_example.wasm $APPS_DIR/host/fixtures/example.wasm
